afdf65bcfb4cd5c562f9fc8f40e4129b39e17563
shin donggyu <donggyu9410@gmail.com>
2020-04-01 18:34:41 +0900

    Added change tests button, see tests button in sidemenunavigation view controller

--- love of midterm/controllers/SideMenuViewController.swift
+++ love of midterm/controllers/SideMenuViewController.swift
@@ -27,6 +27,25 @@ class SideMenuViewController: UIViewController {
         button.setTitle("프로필 변경", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.titleLabel?.font = UIFont(name: "BMJUAOTF", size: 16)
+        button.addTarget(self, action: #selector(changeProfileButtonTapped), for: UIControl.Event.touchUpInside)
+        return button
+    }()
+    
+    lazy var changeTests:UIButton = {
+        let button = UIButton(type: UIButton.ButtonType.system)
+        button.setTitle("문제 변경", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont(name: "BMJUAOTF", size: 16)
+        button.addTarget(self, action: #selector(changeTestsButtonTapped), for: UIControl.Event.touchUpInside)
+        return button
+    }()
+    
+    lazy var seeTests:UIButton = {
+        let button = UIButton(type: UIButton.ButtonType.system)
+        button.setTitle("문제 확인", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont(name: "BMJUAOTF", size: 16)
+        button.addTarget(self, action: #selector(seeTestsButtonTapped), for: UIControl.Event.touchUpInside)
         return button
     }()
     
@@ -42,6 +61,18 @@ class SideMenuViewController: UIViewController {
         dismiss(animated: true, completion: nil)
         try! Auth.auth().signOut()
         RootViewController.rootViewController.logoutFunction()
+    }
+    
+    @objc func changeProfileButtonTapped(){
+        print("changeProfileButtonTapped")
+    }
+    
+    @objc func changeTestsButtonTapped(){
+        print("changeTestsButtonTapped")
+    }
+    
+    @objc func seeTestsButtonTapped(){
+        print("seeTestsButtonTapped")
     }
     
     // MARK: configures
@@ -62,9 +93,23 @@ class SideMenuViewController: UIViewController {
         editProfileButton.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
         
         
+        view.addSubview(changeTests)
+        changeTests.translatesAutoresizingMaskIntoConstraints = false
+        changeTests.topAnchor.constraint(equalTo: editProfileButton.bottomAnchor, constant: 20).isActive = true
+        changeTests.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+        changeTests.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+        
+        
+        
+        view.addSubview(seeTests)
+        seeTests.translatesAutoresizingMaskIntoConstraints = false
+        seeTests.topAnchor.constraint(equalTo: changeTests.bottomAnchor, constant: 20).isActive = true
+        seeTests.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+        seeTests.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+        
         view.addSubview(logoutButton)
         logoutButton.translatesAutoresizingMaskIntoConstraints = false
-        logoutButton.topAnchor.constraint(equalTo: editProfileButton.bottomAnchor, constant: 20).isActive = true
+        logoutButton.topAnchor.constraint(equalTo: seeTests.bottomAnchor, constant: 20).isActive = true
         logoutButton.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         logoutButton.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
     }
